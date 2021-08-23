7814b3b9a5301cbaa217bdfffed8e841928dcc3d
ataa-1312 <79143733+ataa-1312@users.noreply.github.com>
2021-08-24 01:01:25 +0300

    Seventh Commit/Errors added

--- Internship Project.py
+++ Internship Project.py
@@ -1,15 +1,11 @@
-import sys
-import os
-from typing import Counter
+import sys, os, re
 from PySide6 import QtGui, QtCore
 from PySide6.QtWidgets import QApplication, QComboBox, QLabel, QMainWindow, QPlainTextEdit, QVBoxLayout, QFrame
-from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QDialogButtonBox, QFileDialog, QGridLayout
-from PySide6.QtCore import Qt, QTimer
+from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QDialogButtonBox, QFileDialog, QGridLayout, QMessageBox
+from PySide6.QtCore import Qt
 from PIL import Image, ImageDraw, ImageFont
 from functools import partial
-import re
 import numpy as np
-
 
 
 class MainWin(QMainWindow):
@@ -18,7 +14,7 @@ class MainWin(QMainWindow):
 
         # General Setup
         self.setWindowTitle("Photo Editor")
-        self.setFixedSize(400, 450)
+        self.setFixedSize(420, 470)
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
         self.generalLayout = QVBoxLayout()
@@ -165,60 +161,77 @@ class MainWinCtrl():
             self._view.destDisplay.setText(self.folderName)
 
     def crop_and_merge(self):
+        # Error Handling
+        if self._view.srcDisplay.text() == '':
+            QMessageBox.critical( None, 'Source address missing', 'Please enter a directory.')
+        elif self._view.destDisplay.text() == '': 
+            QMessageBox.critical( None, 'Destination address missing', 'Please enter a directory.')
+        elif self._view.fileName.text() == '':
+            QMessageBox.critical(None, 'File Name missing', 'Please enter a Name.')
+        elif self._view.yCoordinate.text() == '':
+            QMessageBox.critical(None, 'Y-coordinate missing', 'Please enter a Name.')
+        else:
         # Crop
-        srcAddress = self._view.srcDisplay.displayText()
-        destAddress = self._view.destDisplay.displayText()
-        name = self._view.fileName.displayText()
-        y_cord = int(self._view.yCoordinate.displayText())
-        os.chdir(r'{}'.format(srcAddress))
-        images = [Image.open(image) for image in os.listdir()]
-        for image in images[0:-1]:
-            images[images.index(image)] = image.crop(
-                (0, 0, y_cord, self.SIZE[1]))
-        #------------------------------------------------------------------------#
-        # Merge
-        widths, heights = zip(*(image.size for image in images))
-        total_width = sum(widths)
-        max_height = max(heights)
-        self.canvas = Image.new("RGB", size=(
-            total_width, max_height + 50), color=(255, 255, 255))
-        x_offset = 0
+            srcAddress = self._view.srcDisplay.displayText()
+            destAddress = self._view.destDisplay.displayText()
+            name = self._view.fileName.displayText()
+            y_cord = int(self._view.yCoordinate.displayText())
+            os.chdir(r'{}'.format(srcAddress))
+            images = [Image.open(image) for image in os.listdir()]
+            for image in images[0:-1]:
+                images[images.index(image)] = image.crop(
+                    (0, 0, y_cord, self.SIZE[1]))
+            #------------------------------------------------------------------------#
+            # Merge
+            widths, heights = zip(*(image.size for image in images))
+            total_width = sum(widths)
+            max_height = max(heights)
+            self.canvas = Image.new("RGB", size=(
+                total_width, max_height + 50), color=(255, 255, 255))
+            x_offset = 0
 
-        for image in images:
-            self.canvas.paste(image, (x_offset, 0))
-            x_offset += image.size[0]
+            for image in images:
+                self.canvas.paste(image, (x_offset, 0))
+                x_offset += image.size[0]
 
-        os.chdir(r'{}'.format(destAddress))
-        self.canvas.save(f"{name}.png")
+            os.chdir(r'{}'.format(destAddress))
+            self.canvas.save(f"{name}.png")
 
     def box_and_annotate(self):
-        self.destAddress = self._view.destDisplay.displayText()
-        self.name = self._view.fileName.displayText()
-        self.image  = Image.open(r'{}'.format(self.destAddress + f'/{self.name}.png'))
-        self.strt_time = self._view.startTime.displayText()
-        self.end_time = self._view.endTime.displayText()
-        self.text = self._view.plainText.toPlainText()
-        self.color = str(self._view.colorCombo.currentText()).lower()
-        
-        strt_pix = self.time_to_pix(self.strt_time)
-        end_pix =  self.time_to_pix(self.end_time)
-        
-        frame = (strt_pix, 0, end_pix, self.SIZE[1])
-        # Box
-        draw = ImageDraw.Draw(self.image)
-        draw.rectangle(frame, outline = self.COLORS[self.color], width=3)
-        #Annotate
-        text_font = ImageFont.truetype(r'‪C:\Windows\Fonts\arial.ttf', size=11)
-        draw.text((strt_pix, self.SIZE[1] + 10), self.text, self.color, text_font)
-        self.image.save(f"{self.name}.png")
-        self._view.startTime.clear()
-        self._view.endTime.clear()
-        self._view.plainText.clear()
+        #Error
+        if self._view.fileName.text() == '':
+            QMessageBox.critical( None, 'File Name missing', 'Please enter a Name.')
+        elif self._view.startTime.text() == '':
+            QMessageBox.critical(None, 'Start Time Missing ', 'Please enter a Start Time.')
+        elif self._view.endTime.text() == '':
+            QMessageBox.critical(None, 'Start Time Missing ', 'Please enter an End Time.')
+        elif self.time_to_pix(self._view.startTime.text()) > self.time_to_pix(self._view.endTime.text()):
+            QMessageBox.critical(None, 'Time Paradox', 'Start Time must be earlier than End Time.')
+        else:
+            self.destAddress = self._view.destDisplay.displayText()
+            self.name = self._view.fileName.displayText()
+            self.image  = Image.open(r'{}'.format(self.destAddress + f'/{self.name}.png'))
+            self.strt_time = self._view.startTime.displayText()
+            self.end_time = self._view.endTime.displayText()
+            self.text = self._view.plainText.toPlainText()
+            self.color = str(self._view.colorCombo.currentText()).lower()
+            
+            #Starting 
+            strt_pix = self.time_to_pix(self.strt_time)
+            end_pix =  self.time_to_pix(self.end_time)
+            
+            frame = (strt_pix, 0, end_pix, self.SIZE[1])
+            # Box
+            draw = ImageDraw.Draw(self.image)
+            draw.rectangle(frame, outline = self.COLORS[self.color], width=3)
+            #Annotate
+            text_font = ImageFont.truetype(r'‪C:\Windows\Fonts\arial.ttf', size=11)
+            draw.text((strt_pix, self.SIZE[1] + 10), self.text, self.color, text_font)
+            self.image.save(f"{self.name}.png")
+            self._view.startTime.clear()
+            self._view.endTime.clear()
+            self._view.plainText.clear()
 
-
-
-        
-        
     def time_to_pix(self, times):
         time = re.match(r'(?P<Hour>\d{1,2}):(?P<Minute>\d{1,2}):(?P<Second>\d{1,2})', times).groupdict()
         seconds = int(time['Hour']) * 3600 + int(time['Minute']) * 60 + int(time['Second'])
