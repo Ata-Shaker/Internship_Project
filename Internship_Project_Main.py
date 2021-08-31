3f67790221563a0087ddee1a2c67de1c59b69700
ataa-1312 <79143733+ataa-1312@users.noreply.github.com>
2021-08-31 23:31:01 +0300

    1st Commit of Modular GUI

--- Internship_Project_Main.py
+++ Internship_Project_Main.py
@@ -1,154 +1,141 @@
-import Internship_Project_Ctrl
-import sys, os
-from PySide6 import QtGui, QtCore
-from PySide6.QtWidgets import QApplication, QComboBox, QLabel, QMainWindow, QPlainTextEdit, QVBoxLayout, QFrame
-from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QDialogButtonBox, QFileDialog, QGridLayout, QMessageBox
-from PySide6.QtCore import Qt
+import sys
+from PySide6 import QtGui
+from PySide6.QtWidgets import QApplication, QComboBox, QDateTimeEdit, QDialogButtonBox, QGroupBox, QLabel, QMainWindow, QRadioButton
+from PySide6.QtWidgets import QGridLayout, QPushButton, QTabWidget, QVBoxLayout, QWidget, QLineEdit
+from PySide6.QtCore import QDate, QDateTime, Qt 
 
-
-class MainWin(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        # General Setup
-        self.setWindowTitle("Photo Editor")
-        self.setFixedSize(600, 550)
-        self._centralWidget = QWidget(self)
-        self.setCentralWidget(self._centralWidget)
-        self.generalLayout = QVBoxLayout()
-        self.generalLayout.setAlignment(Qt.AlignTop)
-
-        # -----Beginning of First Layout-----
-        self.firstLayout = QGridLayout()
-
-        self.srcPathLabel = QLabel('Source Path:')
-        self.firstLayout.addWidget(self.srcPathLabel, 0, 0)
-
-        self.srcDisplay = QLineEdit()
-        self.srcDisplay.setAlignment(Qt.AlignCenter)
-        self.srcDisplay.setReadOnly(True)
-        self.browseButton1 = QPushButton('Browse')
-        self.firstLayout.addWidget(self.srcDisplay, 1, 0, 1, 5)
-        self.firstLayout.addWidget(self.browseButton1, 1, 5, 1, 1)
-
-        self.destPathLabel = QLabel('Destination Path:')
-        self.firstLayout.addWidget(self.destPathLabel, 2, 0)
-
-        self.destDisplay = QLineEdit()
-        self.destDisplay.setAlignment(Qt.AlignCenter)
-        self.destDisplay.setReadOnly(True)
-        self.browseButton2 = QPushButton('Browse')
-        self.firstLayout.addWidget(self.destDisplay, 3, 0, 1, 5)
-        self.firstLayout.addWidget(self.browseButton2, 3, 5, 1 ,1)
-
-        self.fileNameLabel = QLabel('File Name:')
-        self.firstLayout.addWidget(self.fileNameLabel, 4, 0, 1, 3)
-        self.yCoordinateLabel = QLabel('Y Coordinate:')
-        self.firstLayout.addWidget(self.yCoordinateLabel, 4, 3, 1, 3)
+class MainWin(QMainWindow): 
+    def __init__(self): 
+        super().__init__() 
+ 
+        self.setWindowTitle('New Photo Editor') 
+        self.setFixedSize(360, 435) 
+        self.centralWidget = QWidget(self) 
+        self.setCentralWidget(self.centralWidget) 
+ 
+        self.general_Layout = QVBoxLayout(parent = self.centralWidget) 
+        self.innerlayout_1 = QGridLayout() 
+        self.innerlayout_1.setAlignment(Qt.AlignTop) 
         
-        self.fileName = QLineEdit()
-        self.fileName.setPlaceholderText('Enter the Name')
-        self.fileName.setAlignment(Qt.AlignCenter)
-        self.firstLayout.addWidget(self.fileName, 5, 0, 1, 3)
+        #----------------------------Mutual Setup Start---------------------------#
+        self.addSourceWidgets() 
+        self.addDestinationWidgets() 
+        self.addFileWidgets() 
+        self.general_Layout.addLayout(self.innerlayout_1)
+        #-----------------------------Mutual Setup End----------------------------#
 
-        self.yCoordinate = QLineEdit() 
-        self.yCoordinate.setPlaceholderText('Enter the Y Coordinate')
-        self.yCoordinate.setAlignment(Qt.AlignCenter)
-        self.yCoordinate.setValidator(QtGui.QIntValidator(bottom = 0))
-        self.firstLayout.addWidget(self.yCoordinate, 5, 3, 1, 3 )
+        self.createTabsWidgets() #-----> Creating Tabs
 
-        self.recordStartTimeLabel = QLabel('Record Start Time:')
-        self.firstLayout.addWidget(self.recordStartTimeLabel, 6, 0, 1, 3)
-        self.singularTimeLengthLabel = QLabel('Duration of Each File: (Seconds)')
-        self.firstLayout.addWidget(self.singularTimeLengthLabel, 6, 3, 1, 3)
+        #----------------------------Merge Tab Start---------------------------#
+        self.mergeTabMain_Layout = QVBoxLayout(parent=self.mergeTab)
+        self.addCalendarToMergeTab()
+        self.addIntervalOptionsToMergeTab()
+        self.mergeTab.setLayout(self.mergeTabMain_Layout)
+        #-----------------------------Merge Tab End----------------------------#
 
-        self.recordStartTime = QLineEdit()
-        self.recordStartTime.setPlaceholderText('HH:MM:SS')
+        self.closeButton = QDialogButtonBox(QDialogButtonBox.Close, parent = self)
+        self.general_Layout.addWidget(self.closeButton)
+
+        self.centralWidget.setLayout(self.general_Layout)
+
+    def addSourceWidgets(self): 
+        self.sourceDisplay_Label = QLabel(parent = self.centralWidget, text = 'Source Path:') 
+        self.innerlayout_1.addWidget(self.sourceDisplay_Label, 0, 0) # --> Label 
+ 
+        self.sourceDisplay = QLineEdit(parent = self.centralWidget) 
+        self.sourceDisplay.setAlignment(Qt.AlignCenter) 
+        self.sourceDisplay.setReadOnly(True)    
+        self.innerlayout_1.addWidget(self.sourceDisplay, 1, 0, 1, 5) # --> Display 
+ 
+        self.sourceBrowseButton = QPushButton(parent = self.centralWidget, text= 'Browse') 
+        self.innerlayout_1.addWidget(self.sourceBrowseButton, 1, 5, 1, 1) # --> Button 
+ 
+    def addDestinationWidgets(self): 
+        self.destinationDisplay_Label = QLabel(parent = self.centralWidget, text = 'Destination Path:') 
+        self.innerlayout_1.addWidget(self.destinationDisplay_Label, 2, 0) # --> Label 
+ 
+        self.destinationDisplay = QLineEdit(parent = self.centralWidget) 
+        self.destinationDisplay.setAlignment(Qt.AlignCenter) 
+        self.destinationDisplay.setReadOnly(True)    
+        self.innerlayout_1.addWidget(self.destinationDisplay, 3, 0, 1, 5 )# --> Display 
+ 
+        self.destinationBrowseButton = QPushButton(parent = self.centralWidget, text= 'Browse') 
+        self.innerlayout_1.addWidget(self.destinationBrowseButton, 3, 5, 1, 1) # --> Button 
+ 
+    def addFileWidgets(self): 
+        self.fileName_Label = QLabel(parent = self.centralWidget, text ='File Name:') 
+        self.innerlayout_1.addWidget(self.fileName_Label, 4, 0, 1, 3) # --> File Name Label 
+ 
+        self.fileType_Label = QLabel(parent = self.centralWidget, text ='File Type:') 
+        self.innerlayout_1.addWidget(self.fileType_Label, 4, 3, 1, 3) # --> File Type Label 
+ 
+        self.fileName = QLineEdit(parent = self.centralWidget) 
+        self.fileName.setPlaceholderText('Enter the File Name') 
+        self.fileName.setAlignment(Qt.AlignCenter) 
+        self.innerlayout_1.addWidget(self.fileName, 5, 0, 1, 3) # --> File Name Widget 
+ 
+        self.fileType = QComboBox(parent = self.centralWidget) 
+        self.fileType.addItems(['JPEG', 'PNG', 'BMP']) 
+        self.innerlayout_1.addWidget(self.fileType, 5, 3, 1, 3) # # --> File Type Widget 
+ 
+    def createTabsWidgets(self):
+        self.tabs = QTabWidget(self.centralWidget)
+        self.mergeTab = QWidget()
+        self.annotateTab = QWidget()
+        self.tabs.addTab(self.mergeTab, 'Merge')
+        self.tabs.addTab(self.annotateTab, 'Annotate')
+        self.general_Layout.addWidget(self.tabs)
+
+    def addCalendarToMergeTab(self):
+        self.mergeTab_Layout = QGridLayout()
+        self.mergeTab_Layout.setAlignment(Qt.AlignTop)
+        
+        self.recordStartTime_Label = QLabel(parent = self.mergeTab , text = 'Record Start Time:')
+        self.mergeTab_Layout.addWidget(self.recordStartTime_Label, 0, 0)
+
+        #---------------------------Calendar Start-------------------------------#
+        self.recordStartTime = QDateTimeEdit(QDate.currentDate(), parent = self.mergeTab)
+        self.recordStartTime.setMaximumDate(QDate.currentDate().addDays(365))
+        self.recordStartTime.setMinimumDate(QDate.currentDate().addDays(-365))
+        self.recordStartTime.setCalendarPopup(True)
+        self.recordStartTime.setDisplayFormat('yyyy/MM/dd HH:mm')
         self.recordStartTime.setAlignment(Qt.AlignCenter)
-        self.recordStartTime.setValidator(QtGui.QRegularExpressionValidator(r"[0-1]{0,1}[0-9]{1}:[0-5]{0,1}[0-9]{1}:[0-5]{0,1}[0-9]{1}|[2]{1}[0-4]{1}:[0-5]{0,1}[0-9]{1}:[0-5]{0,1}[0-9]{1}"))
-        self.firstLayout.addWidget(self.recordStartTime, 7, 0, 1, 3)
+        self.mergeTab_Layout.addWidget(self.recordStartTime)
+        self.mergeTabMain_Layout.addLayout(self.mergeTab_Layout)
+        #---------------------------Calendar End---------------------------------#
 
-        self.singularTimeLength = QLineEdit()
-        self.singularTimeLength.setPlaceholderText('Enter the Duration of Each Image in Seconds')
-        self.singularTimeLength.setAlignment(Qt.AlignCenter)
-        self.singularTimeLength.setValidator(QtGui.QIntValidator(bottom = 0))
-        self.firstLayout.addWidget(self.singularTimeLength, 7, 3, 1, 3)
+    def addIntervalOptionsToMergeTab(self):
+        #---------------------------Display Interval Start-----------------------#
+        self.dateTimeDiplayInterval = QGroupBox(parent= self.mergeTab, title = 'Display Interval:')
+        self.thirtyMin = QRadioButton(parent = self.dateTimeDiplayInterval, text = '&3&0 Minutes')
+        self.oneHour = QRadioButton(parent = self.dateTimeDiplayInterval, text = '&1 Hour')
+        self.ninetyMin = QRadioButton(parent = self.dateTimeDiplayInterval, text = '&9&0 Minutes')
+        self.twoHour = QRadioButton(parent = self.dateTimeDiplayInterval, text = '&2 Hours')
+        self.thirtyMin.setChecked(True)
 
-        self.runButton1 = QPushButton('Merge')
-        self.runButton1.setFixedHeight(25)
-        self.firstLayout.addWidget(self.runButton1, 8, 0, 1, 6)
+        self.dateTimeDiplayInterval_Layout = QVBoxLayout(self.dateTimeDiplayInterval)
+        self.dateTimeDiplayInterval_Layout.addWidget(self.thirtyMin)
+        self.dateTimeDiplayInterval_Layout.addWidget(self.oneHour)
+        self.dateTimeDiplayInterval_Layout.addWidget(self.ninetyMin)
+        self.dateTimeDiplayInterval_Layout.addWidget(self.twoHour)
 
-        self.generalLayout.addLayout(self.firstLayout)
-
-        # -----End of First Layout-----
-
-        self.generalLayout.addSpacing(6)
-        self.horizontalLine = QFrame()
-        self.horizontalLine.setFrameShape(QFrame.HLine)
-        self.horizontalLine.setFrameShadow(QFrame.Sunken)
-        self.generalLayout.addWidget(self.horizontalLine)     
-
-        # -----Beginning of Second Layout-----
-        self.secondLayout = QGridLayout()
-
-        # Labels
-        self.startTimeLabel = QLabel('Start Time:')
-        self.endTimeLabel = QLabel('End Time:')
-        self.colorComboLabel = QLabel('Color:')
-        self.secondLayout.addWidget(self.startTimeLabel, 0, 0, 1, 2)
-        self.secondLayout.addWidget(self.endTimeLabel, 0, 2, 1, 2)
-        self.secondLayout.addWidget(self.colorComboLabel, 0, 4, 1, 1)
-
-        # Buttons
-        self.startTime = QLineEdit()
-        self.startTime.setPlaceholderText('HH:MM:SS')
-        self.startTime.setValidator(QtGui.QRegularExpressionValidator(r"[0-9]{1,2}:[0-5]{0,1}[0-9]{1}:[0-5]{0,1}[0-9]{1}"))
-        self.startTime.setAlignment(Qt.AlignCenter)
-        self.endTime = QLineEdit()
-        self.endTime.setPlaceholderText('HH:MM:SS')
-        self.endTime.setValidator(QtGui.QRegularExpressionValidator(r"[0-9]{1,2}:[0-5]{0,1}[0-9]{1}:[0-5]{0,1}[0-9]{1}"))
-        self.endTime.setAlignment(Qt.AlignCenter)
-        self.colorCombo = QComboBox()
-        self.colorCombo.addItem('Black')
-        self.colorCombo.addItem('White')
-        self.colorCombo.addItem('Green')
-        self.colorCombo.addItem('Red')
-        self.colorCombo.addItem('Blue')
-        self.colorCombo.addItem('Yellow')
-        self.secondLayout.addWidget(self.startTime, 1, 0, 1, 2)
-        self.secondLayout.addWidget(self.endTime, 1, 2, 1, 2)
-        self.secondLayout.addWidget(self.colorCombo, 1, 4, 1, 1)
-
-
-        self.plainTextLabel = QLabel('Comment:')
-        self.secondLayout.addWidget(self.plainTextLabel, 2, 0)
-        self.plainText = QPlainTextEdit()
-        self.plainText.setPlaceholderText('Enter Comment')
-        self.secondLayout.addWidget(self.plainText, 3, 0, 1, 5)
-
-        self.runButton2 = QPushButton('Annotate')
-        self.secondLayout.addWidget(self.runButton2, 4, 0, 1, 5)
-
-        self.generalLayout.addLayout(self.secondLayout)
-        #-----End of Second Layout
-
-
-        self.stdBtns = QDialogButtonBox()
-        self.stdBtns.setStandardButtons(QDialogButtonBox.Close)
-        self.generalLayout.addWidget(self.stdBtns)
-
+        self.dateTimeDiplayInterval.setLayout(self.dateTimeDiplayInterval_Layout)
+        self.mergeTabMain_Layout.addWidget(self.dateTimeDiplayInterval)
+        #---------------------------Display Interval End-------------------------#
         
-        self._centralWidget.setLayout(self.generalLayout)
-    
-       
+        self.mergeButton = QPushButton(parent = self.mergeTab, text = 'Merge')
+        self.mergeTabMain_Layout.addWidget(self.mergeButton)
+
+
+
+
+    #     self.mergeTab.setLayout(self.mergeTab_Layout)
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication()
     GUI = MainWin()
-    Ctrl = Internship_Project_Ctrl.MainWinCtrl(GUI)
     GUI.show()
     sys.exit(app.exec())
-
-
-if __name__ == "__main__":
-    main()  
+        
+if __name__ == '__main__':
+    main()
