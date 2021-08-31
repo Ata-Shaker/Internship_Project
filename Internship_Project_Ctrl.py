fb26bc50e6cbd09116d36081e55a1494d9ec2ed7
ataa-1312 <79143733+ataa-1312@users.noreply.github.com>
2021-08-31 09:21:26 +0300

    +3th Commit - Have added Tabs

--- Internship_Project_Ctrl.py
+++ Internship_Project_Ctrl.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import  QFileDialog, QMessageBox
 from PySide6.QtCore import Qt
 from PIL import Image, ImageDraw, ImageFont
 from functools import partial
+from datetime import datetime, timedelta
 import numpy as np
 
 
@@ -12,16 +13,9 @@ class MainWinCtrl():
     def __init__(self, view):
         self._view = view
         self.SIZE = (1878, 636)  # May be Automated
-        self.PIX_PER_SEC = 2.92  # Subject to Change. May need a funtion.
         self.COLORS = {"black": (0, 0, 0), "white": (255, 255, 255), "blue": (0, 0, 255), "red": (255, 0, 0), "green": (0, 255, 0),
                        "yellow": (255, 255, 0)}
-        self.destAddress = None
-        self.name = None
-        self.strt_time = None
-        self.end_time = None
-        self.text = None
-        self.color = None
-
+        self.text_font = ImageFont.truetype(r'‪C:\Windows\Fonts\arial.ttf', size=11)
         self._connectSignals()
 
     def _connectSignals(self):
@@ -55,9 +49,10 @@ class MainWinCtrl():
             name = self._view.fileName.displayText()
             yCord = int(self._view.yCoordinate.displayText())
 
-            recordStart = self._view.recordStartStime.text()
+            recordStart = self._view.recordStartTime.text()
             recordStartDic = re.match(r'(?P<Hour>\d{1,2}):(?P<Minute>\d{1,2}):(?P<Second>\d{1,2})', recordStart).groupdict()
-            #recordStart_datetime = 
+            recordStart_datetime = datetime(1,1,1,int(recordStartDic['Hour']),int(recordStartDic['Minute']),int(recordStartDic['Second']))
+            thirtyMinutes = timedelta(minutes=30)
             os.chdir(r'{}'.format(srcAddress))
 
             imageNameList = [re.findall(r'^.+\.png|^.+\.jpg|^.+\.jpeg', imageName)[0] for imageName in os.listdir() if re.findall(r'^.+\.png|^.+\.jpg|^.+\.jpeg', imageName) != []]
@@ -72,26 +67,36 @@ class MainWinCtrl():
             #------------------------------------------------------------------------#
             # Merge
             widths, heights = zip(*(image.size for image in images))
-            total_width = sum(widths)
+            self.total_width = sum(widths)
             max_height = max(heights)
             self.canvas = Image.new("RGB", size=(
-                total_width, max_height + 80), color=(255, 255, 255))
+                self.total_width, max_height + 80), color=(255, 255, 255))
+            
             x_offset = 0
-
             for image in images:
                 self.canvas.paste(image, (x_offset, 30))
                 x_offset += image.size[0]
-                if x_offset % (self.PIX_PER_SEC * 1800) == 0:
-                    pass
+            #------------------------------------------------------------------------#
+
+            x_offset = 0
+            canvasDraw = ImageDraw.Draw(self.canvas)
+            while x_offset < self.total_width:
+                canvasDraw.text((x_offset, 15), recordStart_datetime.strftime('%H:%M:%S'), self.COLORS['black'], self.text_font )
+                x_offset += 1800 * self.pix_per_sec()
+                recordStart_datetime += thirtyMinutes
+                    
+
 
             os.chdir(r'{}'.format(destAddress))
-            self.canvas.save(f"{name}.png")
+            self.canvas.save(f"{name}.jpeg")
             QMessageBox.information(None, 'Info', 'Done!')
 
     def box_and_annotate(self):
         #Error
-        if self._view.fileName.text() == '':
-            QMessageBox.critical( None, 'File Name missing', 'Please enter a Name.')
+        if self._view.destDisplay.text() == '':
+            QMessageBox.critical( None, 'Destination Path Missing', 'Please enter a Destination Path.')
+        elif self._view.fileName.text() == '':
+            QMessageBox.critical( None, 'File Name Missing', 'Please enter a Name.')
         elif self._view.startTime.text() == '':
             QMessageBox.critical(None, 'Start Time Missing ', 'Please enter a Start Time.')
         elif self._view.endTime.text() == '':
@@ -105,8 +110,9 @@ class MainWinCtrl():
             self.end_time = self._view.endTime.displayText()
             self.text = self._view.plainText.toPlainText()
             self.color = str(self._view.colorCombo.currentText()).lower()
+            
             try:
-                self.image  = Image.open(r'{}'.format(self.destAddress + f'/{self.name}.png'))
+                self.image  = Image.open(r'{}'.format(self.destAddress + f'/{self.name}.jpeg'))
             except:
                 QMessageBox.critical(None, 'File Non-Existant', 'Make sure the File Name you provided is valid.')
             else:
@@ -114,22 +120,27 @@ class MainWinCtrl():
                 #Starting 
                 strt_pix = self.time_to_pix(self.strt_time)
                 end_pix =  self.time_to_pix(self.end_time)
-                
-                frame = (strt_pix, 30, end_pix, self.SIZE[1] + 30)
-                # Box
-                draw = ImageDraw.Draw(self.image)
-                draw.rectangle(frame, outline = self.COLORS[self.color], width=3)
-                #Annotate
-                text_font = ImageFont.truetype(r'‪C:\Windows\Fonts\arial.ttf', size=11)
-                draw.text((strt_pix, self.SIZE[1] + 10), self.text, self.color, text_font)
-                self.image.save(f"{self.name}.png")
-                self._view.startTime.clear()
-                self._view.endTime.clear()
-                self._view.plainText.clear()
-                QMessageBox.information(None, 'Info', 'Done!')
+
+                if strt_pix > self.image.size[0] or end_pix > self.image.size[0]:
+                    QMessageBox.critical(None, 'Time Paradox', 'Start Time and End Time must be within the limits.')
+                else:
+                    frame = (strt_pix, 30, end_pix, self.SIZE[1] + 30)
+                    # Box
+                    draw = ImageDraw.Draw(self.image)
+                    draw.rectangle(frame, outline = self.COLORS[self.color], width=3)
+                    #Annotate
+                    draw.text((strt_pix, self.SIZE[1] + 10), self.text, self.color, self.text_font)
+                    self.image.save(f"{self.name}.jpeg")
+                    self._view.startTime.clear()
+                    self._view.endTime.clear()
+                    self._view.plainText.clear()
+                    QMessageBox.information(None, 'Info', 'Done!')
 
 
     def time_to_pix(self, times):
         time = re.match(r'(?P<Hour>\d{1,2}):(?P<Minute>\d{1,2}):(?P<Second>\d{1,2})', times).groupdict()
         seconds = int(time['Hour']) * 3600 + int(time['Minute']) * 60 + int(time['Second'])
-        return np.round(seconds * self.PIX_PER_SEC)  # May not need the round function
+        return np.round(seconds * self.pix_per_sec())  # May not need the round function
+
+    def pix_per_sec(self):
+        return float(self._view.yCoordinate.text())/float(self._view.singularTimeLength.text())
